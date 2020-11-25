9b076b2c0c6f1165f82a319460a60a3156e69f81
Dhananjay Tiwari -X (dhantiwa - WIPRO LIMITED at Cisco) <dhantiwa@partnet.cisco.com>
2020-11-26 00:22:22 +0530

    uppdated the heroku and netlify location

--- src/config/index.js
+++ src/config/index.js
@@ -2,14 +2,14 @@ const providers = ['github']
 
 const callbacks = providers.map(provider => {
   return process.env.NODE_ENV === 'production'
-    ? `https://node-github-auth-server.herokuapp.com/${provider}/callback`
+    ? `https://pinns-api-server.herokuapp.com/${provider}/callback`
     : `https://localhost:8080/${provider}/callback`
 })
 
 const [githubURL] = callbacks
 
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://react-github-auth-client.netlify.com'
+  ? 'https://pinns-ui-app.netlify.com'
   : ['https://127.0.0.1:3000', 'https://localhost:3000']
 
 exports.GITHUB_CONFIG = {
